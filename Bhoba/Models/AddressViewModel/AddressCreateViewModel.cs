b2532e9973bd277848056656cc94a055f6b36370
Figamus <fig74656@yahoo.com>
2018-12-14 14:59:08 -0600

    Address creates address on table and makes a felonaddress join upon creation based on felon Id

---
+++ Bhoba/Models/AddressViewModel/AddressCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bhoba.Models.AddressViewModel
+{
+    public class AddressCreateViewModel
+    {
+        public Address Address { get; set; }
+        public Felon Felon { get; set; }
+    }
+}
