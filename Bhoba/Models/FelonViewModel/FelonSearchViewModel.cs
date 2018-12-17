a36d4c267451f75c9429c8bc937d21e90561e389
Figamus <fig74656@yahoo.com>
2018-12-17 09:12:42 -0600

    Search can search first name, last name, or both

---
+++ Bhoba/Models/FelonViewModel/FelonSearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bhoba.Models.FelonViewModel
+{
+    public class FelonSearchViewModel
+    {
+        public string Search { get; set; }
+
+        public List<Felon> Felons { get; set; }
+    }
+}
